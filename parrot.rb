57f54dbb41c6b51f5b9ff90a8410554611d9da2f
Marcus Lau <lau.marcus76@gmail.com>
2020-04-15 15:38:26 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(noise="Squawk!")
-  puts "#{noise}"
+def parrot(name, noise="Squawk!")
+  puts "#{name}"
 end
   
